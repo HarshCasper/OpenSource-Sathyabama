fc74ab2c150b04f53448b0c6cdfa582a4681b52d
Harsh Bardhan Mishra <47351025+HarshCasper@users.noreply.github.com>
2019-10-16 15:37:32 +0530

    Create pointers-in-c.c

---
+++ C/Hackerrank Solutions/Genin/pointers-in-c.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void update(int *a,int *b) {
+    *a=*a+*b;
+    *b=abs(*a-(2**b));   
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
+
