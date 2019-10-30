563657139b4775db22ae4518551a2ab7a5342870
Harsh Bardhan Mishra <47351025+HarshCasper@users.noreply.github.com>
2019-10-30 10:39:21 +0530

    Create Day-0.c

---
+++ 30 Days of Code/Day-0.c
@@ -0,0 +1,11 @@
+// https://www.hackerrank.com/challenges/30-hello-world/problem
+
+#include <stdio.h>
+int main()
+{
+   char str[100]; 
+   gets(str); 
+  system("cls");
+  printf ("Hello, World." "\n" ); 
+printf ( "%s",str );
+}
