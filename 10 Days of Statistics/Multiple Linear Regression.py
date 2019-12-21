7efd030a267c57f2571bec9420b219607e3d8ee7
Harsh Bardhan Mishra <47351025+HarshCasper@users.noreply.github.com>
2019-12-21 19:39:20 +0530

    Create Multiple Linear Regression.py

---
+++ 10 Days of Statistics/Multiple Linear Regression.py
@@ -0,0 +1,40 @@
+# https://www.hackerrank.com/challenges/s10-multiple-linear-regression/problem
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from sklearn import linear_model
+
+# Set data
+m, n = map(int, input().split())
+X, Y = [], []
+
+# Get the parameters X and Y for discovery the variables a and b
+for i in range(n):
+    x = [0]
+    elements = list(map(float, input().split()))
+    for j in range(len(elements)):
+        if j < m:
+            x.append(elements[j])
+        else:
+            Y.append(elements[j])
+    X.append(x)
+
+# Set the model LinearRegression
+model = linear_model.LinearRegression()
+model.fit(X, Y)
+a = model.intercept_
+b = model.coef_
+
+# Get the parameters X for discovery the Y
+q = int(input())
+new_X = []
+for i in range(q):
+    x = [0]
+    elements = list(map(float, input().split()))
+    for j in range(len(elements)):
+        x.append(elements[j])
+    new_X.append(x)
+
+# Gets the result and show on the screen
+result = model.predict(new_X)
+for i in range(len(result)):
+    print(round(result[i],2))
