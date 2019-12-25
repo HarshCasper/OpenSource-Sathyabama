88de31edbe8437aff4039c72376f16f5b6ffad18
Harsh Bardhan Mishra <47351025+HarshCasper@users.noreply.github.com>
2019-12-25 14:53:22 +0530

    Create Ruby Hash - Initialization.rb

---
+++ Ruby/Hackerrank Challenges/Ruby Hash - Initialization.rb
@@ -0,0 +1,14 @@
+# https://www.hackerrank.com/challenges/ruby-hash-initialization/problem
+
+def empty_hash
+  empty_hash = Hash.new 
+end
+
+def default_hash
+  default_hash = Hash.new(1)
+end 
+
+def hackerrank
+  hackerrank = Hash.new
+  hackerrank = {"simmy" => 100, "vivmbbs" => 200}
+end
