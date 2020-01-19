212d56db4a0f1cd2276d90a3b7c7a36804f35f5d
Harsh Bardhan Mishra <47351025+HarshCasper@users.noreply.github.com>
2020-01-19 20:44:48 +0530

    Create The PADS.sql

---
+++ Databases/Hackerrank Problems/The PADS.sql
@@ -0,0 +1,10 @@
+-- https://www.hackerrank.com/challenges/the-pads/problem
+
+SELECT Name || '(' || SUBSTR(Occupation, 1, 1) || ')'
+FROM OCCUPATIONS
+ORDER BY Name;
+
+SELECT 'There are a total of ' || Count(Name) || ' ' || LOWER(Occupation) || 's.'
+FROM Occupations
+GROUP BY Occupation
+ORDER BY Count(Name), Occupation;
